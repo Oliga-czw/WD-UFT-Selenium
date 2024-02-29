131946d75280f022238608fe69d8819a5e4603ef
Oliga-czw <1060941869@qq.com>
2024-02-29 00:01:44 -0800

    update timeout

--- MES_APEM_UFT_Selenium_Auto/MES_APEM_UFT_Selenium_Auto/TestCase/Mobile Cases/997428.cs
+++ MES_APEM_UFT_Selenium_Auto/MES_APEM_UFT_Selenium_Auto/TestCase/Mobile Cases/997428.cs
@@ -26,7 +26,7 @@ namespace MES_APEM_UFT_Selenium_Auto.TestCase
         [TestCategory(CaseState.Accepted)]
         [TestCategory(AutomationTool.UFT_Selenium)]
         [Owner(AutomationEngineer.Ziru)]
-        [Timeout(600000)]
+        [Timeout(1200000)]
 
 
         [TestMethod]
