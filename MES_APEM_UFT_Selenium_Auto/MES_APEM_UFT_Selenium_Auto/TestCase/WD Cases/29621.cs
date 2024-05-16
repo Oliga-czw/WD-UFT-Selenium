b50477762b0740ec7387ab31e14efd6a1e2d2d28
Oliga-czw <1060941869@qq.com>
2024-05-15 22:32:24 -0700

    add cases
    467297\467298

--- MES_APEM_UFT_Selenium_Auto/MES_APEM_UFT_Selenium_Auto/TestCase/WD Cases/29621.cs
+++ MES_APEM_UFT_Selenium_Auto/MES_APEM_UFT_Selenium_Auto/TestCase/WD Cases/29621.cs
@@ -42,42 +42,47 @@ namespace MES_APEM_UFT_Selenium_Auto.TestCase
             SLM.SLMConfigWindow.Apply.Click();
             //wait for applying
             Thread.Sleep(5000);
-            LogStep(@"2. Open WD web and login");
-            Selenium_Driver driver = new Selenium_Driver(Browser.chrome);
-            Web_Fuction.gotoWDWeb(driver);
-            driver.Wait();
-            Web_Fuction.login();
-            //wait for loading
-            Thread.Sleep(5000);
-            //check error
-            Web_Fuction.TakeScreenshot(Selenium_Driver._Selenium_Driver, Resultpath + "web no license.PNG");
-            string web_error = "Unable to acquire SLM_RN_PME_WghDsp_Web";
-            string message1 = driver.FindElement("//div[@class='gwt-Label License_Block_Msg']").Text;
-            Base_Assert.AreEqual(web_error, message1,"web license error check");
-            driver.Close();
-            LogStep(@"3. Open Wd client");
-            Base_Test.LaunchApp(Base_Directory.WDDir);
-            //wait for loading
-            Thread.Sleep(5000);
-            //check error
-            WD.mainWindow.GetSnapshot(Resultpath + "client no license.PNG");
-            string message2 = WD.LicenseMessageDialog.LicenseLable.Text;
-            string client_error = "Unable to acquire SLM_RN_PME_WghDsp";
-            Base_Assert.AreEqual(client_error, message2, "client license error check");
-            WD.LicenseMessageDialog.OKButton.Click();
-            LogStep(@"4. restone data--add license server");
-            //add
-            SLM.SLMConfigWindow.ServerEdit.SendKeys(LicenseServer);
-            SLM.SLMConfigWindow.AddServer.Click();
-            //wait for adding
-            Thread.Sleep(30000);
-            SLM.SLMConfigWindow.Apply.Click();
-            //wait for applying
-            Thread.Sleep(30000);
-            SLM.SLMConfigWindow.Close();
-            Thread.Sleep(2000);
-            SLM.SLMmainWindow.Close();
-
+            try
+            {
+                LogStep(@"2. Open WD web and login");
+                Selenium_Driver driver = new Selenium_Driver(Browser.chrome);
+                Web_Fuction.gotoWDWeb(driver);
+                driver.Wait();
+                Web_Fuction.login();
+                //wait for loading
+                Thread.Sleep(5000);
+                //check error
+                Web_Fuction.TakeScreenshot(Selenium_Driver._Selenium_Driver, Resultpath + "web no license.PNG");
+                string web_error = "Unable to acquire SLM_RN_PME_WghDsp_Web";
+                string message1 = driver.FindElement("//div[@class='gwt-Label License_Block_Msg']").Text;
+                Base_Assert.AreEqual(web_error, message1,"web license error check");
+                driver.Close();
+                LogStep(@"3. Open Wd client");
+                Base_Test.LaunchApp(Base_Directory.WDDir);
+                //wait for loading
+                Thread.Sleep(5000);
+                //check error
+                WD.mainWindow.GetSnapshot(Resultpath + "client no license.PNG");
+                string message2 = WD.LicenseMessageDialog.LicenseLable.Text;
+                string client_error = "Unable to acquire SLM_RN_PME_WghDsp";
+                Base_Assert.AreEqual(client_error, message2, "client license error check");
+                WD.LicenseMessageDialog.OKButton.Click();
+            }
+            finally
+            {
+                LogStep(@"4. restone data--add license server");
+                //add
+                SLM.SLMConfigWindow.ServerEdit.SendKeys(LicenseServer);
+                SLM.SLMConfigWindow.AddServer.Click();
+                //wait for adding
+                Thread.Sleep(30000);
+                SLM.SLMConfigWindow.Apply.Click();
+                //wait for applying
+                Thread.Sleep(30000);
+                SLM.SLMConfigWindow.Close();
+                Thread.Sleep(2000);
+                SLM.SLMmainWindow.Close();
+            }
         }
 
     }
---
+++ MES_APEM_UFT_Selenium_Auto/MES_APEM_UFT_Selenium_Auto/TestCase/WD Cases/467297.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using MES_APEM_UFT_Selenium_Auto.Library.BaseLibrary;
+using MES_APEM_UFT_Selenium_Auto.Library.SeleniumLibrary;
+using MES_APEM_UFT_Selenium_Auto.Product.WD;
+
+namespace MES_APEM_UFT_Selenium_Auto.TestCase
+{
+    public partial class WD_TestCase
+    {
+        [TestCaseID(467297)]
+        [Title("Deviation -- use 2 scale with different resolution weigh( BOM item weight tolerance 1%)")]
+        [TestCategory(ProductArea.WD)]
+        [Priority(CasePriority.Medium)]
+        [TestCategory(CaseState.Accepted)]
+        [TestCategory(AutomationTool.UFT_Selenium)]
+        [Owner(AutomationEngineer.Ziwei)]
+        [Timeout(600000)]
+
+        [TestMethod]
+        public void VSTS_467297()
+        {
+            string Resultpath = Base_Directory.ResultsDir + CaseID+"-";
+            string xml = "14 aspen wd deviation_467297 bulk load.xml";
+
+            string order = "test1";
+            string material = WDMaterial.X0125;
+            string method = WDMethod.Net;
+            string barcode = "X0125001";
+            string scale1 = "simulator";
+            string scale2 = "simulator001";
+
+            LogStep(@"1. import deviation xml");
+            WD_Fuction.Bulkload(xml);
+            LogStep(@"2. edit scale resolution");
+            Selenium_Driver driver = new Selenium_Driver(Browser.chrome);
+            Web_Fuction.gotoWDWeb(driver);
+            driver.Wait();
+            Web_Fuction.login();
+            driver.Wait();
+            Web_Fuction.gotoTab(WDWebTab.equipment);
+            Thread.Sleep(1000);
+            Web_Fuction.edit_scale(scale1);
+            Web.Equipment_Page.simultor_Resolution.Clear();
+            Web.Equipment_Page.simultor_Resolution.SendKeys("1");
+            Web.Equipment_Page.simultor_name.Click();
+            Web_Fuction.TakeScreenshot(Selenium_Driver._Selenium_Driver, Resultpath + "Scale1.PNG");
+            Web.Equipment_Page.Apply.Click();
+            Thread.Sleep(2000);
+            Web_Fuction.edit_scale(scale2);
+            Web.Equipment_Page.simultor_Resolution.Clear();
+            Web.Equipment_Page.simultor_Resolution.SendKeys("10");
+            Web.Equipment_Page.simultor_name.Click();
+            Web_Fuction.TakeScreenshot(Selenium_Driver._Selenium_Driver, Resultpath + "Scale2.PNG");
+            Web.Equipment_Page.Apply.Click();
+            Thread.Sleep(2000);
+            LogStep(@"3. Active order");
+            Web_Fuction.gotoTab(WDWebTab.order);
+            Web_Fuction.active_order(order);
+            driver.Close();
+            LogStep(@"4. check resolution");
+            Application.LaunchWDAndLogin();
+            WD_Fuction.SelectOrderandMaterial(order, material);
+            WD_Fuction.SelectMehod(method, barcode);
+            WD.mainWindow.ScaleWeightInternalFrame.scale.SelectItems(scale1);
+            Thread.Sleep(1000);
+            WD.mainWindow.GetSnapshot(Resultpath + "Scale1 no insufficient resolution deviation.PNG");
+            Thread.Sleep(1000);
+            WD.mainWindow.ScaleWeightInternalFrame.scale.SelectItems(scale2);
+            Thread.Sleep(1000);
+            WD.mainWindow.GetSnapshot(Resultpath + "Scale2 insufficient resolution deviation.PNG");
+            WD.mainWindow.Dialog.Password.SetSecure(PassWord.qaone1);
+            WD.mainWindow.Dialog.OK.Click();
+            WD.mainWindow.ScaleWeightInternalFrame.cancel.Click();
+            Thread.Sleep(3000);
+            WD_Fuction.Close();
+
+        }
+    }
+}
