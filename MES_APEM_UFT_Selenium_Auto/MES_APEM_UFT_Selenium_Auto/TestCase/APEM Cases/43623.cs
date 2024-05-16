e517211cfe673d58c84775617f42d0a9b6354773
ziru <ziru.huang@aspentech.com>
2024-05-15 19:25:29 -0700

    ziru0516
    update

---
+++ MES_APEM_UFT_Selenium_Auto/MES_APEM_UFT_Selenium_Auto/TestCase/APEM Cases/43623.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MES_APEM_UFT_Selenium_Auto.Library.BaseLibrary;
+using MES_APEM_UFT_Selenium_Auto.Product.APEM;
+using System;
+using HP.LFT.SDK;
+using MES_APEM_UFT_Selenium_Auto.Library.SeleniumLibrary;
+using MES_APEM_UFT_Selenium_Auto.Product.ApemMobile;
+using MES_APEM_UFT_Selenium_Auto.Product.APEM.MOC_TemplatesModule;
+
+namespace MES_APEM_UFT_Selenium_Auto.TestCase
+{
+    public partial class APEM_TestCase
+    {
+        [TestCaseID(43623)]
+        [Title("Effective Date is mandatory for recipe elements if recipe expiration feature is enabled")]
+        [TestCategory(ProductArea.RecipeManagement)]
+        [Priority(CasePriority.Medium)]
+        [TestCategory(CaseState.Accepted)]
+        [TestCategory(AutomationTool.UFT_Selenium)]
+        [Owner(AutomationEngineer.Ziru)]
+        [Timeout(600000)]
+
+        [TestMethod]
+        public void VSTS_43623()
+        {
+            string Resultpath = Base_Directory.ResultsDir + CaseID + "-";
+            string BPLname = "BPL43623";
+            string RPLname = "RPL43623";
+            string MRname = "MR43623";
+            string Configpath = Base_Directory.ConfigDir + "flags.m2r_cfg";
+            string ConfigKey = "EFFECTIVE_RECIPE_VERSION = 0";
+            try
+            {
+                LogStep(@"1. Set key in config file");
+                Base_Function.EditConfigKey(Configpath, ConfigKey);
+                //codify all
+                Base_Test.LaunchApp(Base_Directory.Codify_all);
+                Library.BaseLibrary.Application.LaunchMocAndLogin();
+                APEM.MocmainWindow.BPLDesign.ClickSignle();
+                if (!APEM.MocmainWindow.BPLListInternalFrame.BPLList_Table.Row(BPLname).Existing)
+                {
+                    MOC_TemplatesFunction.Importtemplates("TEMP43623.zip");
+                }
+                //BPLverify
+                APEM.MocmainWindow.BPLDesign.ClickSignle();
+                APEM.MocmainWindow.BPLListInternalFrame.BPLList_Table.Row(BPLname).Click();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.BPLListInternalFrame.VerifyButton.ClickSignle();
+                Thread.Sleep(3000);
+                APEM.MocmainWindow.ConfirmDialog.PasswordEditor.SendKeys(PassWord.qaone1);
+                APEM.MocmainWindow.ConfirmDialog.Reason.SendKeys("Test");
+                APEM.MocmainWindow.ConfirmDialog.OK.Click();
+                DateTime BPLverify = DateTime.Now;
+                // Console.WriteLine(BPLverify);
+                Thread.Sleep(3000);
+
+                //BPLcertify
+                APEM.MocmainWindow.BPLListInternalFrame.BPLList_Table.Row(BPLname).Click();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.BPLListInternalFrame.CertifyButton.Click();
+                Thread.Sleep(3000);
+                APEM.BPLCertifyDialog.YesButton.Click();
+                Thread.Sleep(3000);
+                APEM.MocmainWindow.ConfirmDialog.PasswordEditor.SendKeys(PassWord.qaone1);
+                APEM.MocmainWindow.ConfirmDialog.Reason.SendKeys("Test");
+                APEM.MocmainWindow.ConfirmDialog.OK.Click();
+                DateTime BPLcertify = DateTime.Now;
+                //Console.WriteLine(BPLcertify);
+                Thread.Sleep(3000);
+                APEM.MocmainWindow.BPLDesign.ClickSignle();
+                APEM.MocmainWindow.BPLListInternalFrame.BPLList_Table.Row(BPLname).DoubleClick();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.GetSnapshot(Resultpath + "BPLDate.PNG");
+                DateTime BPLverifydate = DateTime.Parse(APEM.MocmainWindow.BPLDataInternalFrame.verifyDate.Text);
+                DateTime BPLcertifydate = DateTime.Parse(APEM.MocmainWindow.BPLDataInternalFrame.certifyDate.Text);
+                TimeSpan timeDifference1 = BPLverifydate.Subtract(BPLverify);
+                Base_Assert.IsTrue(timeDifference1.TotalSeconds <= 2);
+                TimeSpan timeDifference2 = BPLcertifydate.Subtract(BPLcertify);
+                Base_Assert.IsTrue(timeDifference2.TotalSeconds <= 2);
+
+                //RPLverify
+                APEM.MocmainWindow.RPLDesign.ClickSignle();
+                Thread.Sleep(5000);
+                APEM.MocmainWindow.RPLDesignInternalFrame.RPLListTable.Row(RPLname).Click();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.RPLDesignInternalFrame.VerifyButton.ClickSignle();
+                Thread.Sleep(3000);
+                APEM.VerifyDialog.NoButton.Click();
+                Thread.Sleep(3000);
+                APEM.MocmainWindow.ConfirmDialog.PasswordEditor.SendKeys(PassWord.qaone1);
+                APEM.MocmainWindow.ConfirmDialog.Reason.SendKeys("Test");
+                APEM.MocmainWindow.ConfirmDialog.OK.Click();
+                DateTime RPLverify = DateTime.Now;
+                //Console.WriteLine(RPLverify);
+                Thread.Sleep(3000);
+                //RPLcertify
+                APEM.MocmainWindow.RPLDesign.ClickSignle();
+                Thread.Sleep(5000);
+                APEM.MocmainWindow.RPLDesignInternalFrame.RPLListTable.Row(RPLname).Click();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.RPLDesignInternalFrame.CertifyButton.ClickSignle();
+                Thread.Sleep(3000);
+                APEM.CertifyDialog.YesButton.Click();
+                Thread.Sleep(3000);
+                APEM.MocmainWindow.ConfirmDialog.PasswordEditor.SendKeys(PassWord.qaone1);
+                APEM.MocmainWindow.ConfirmDialog.Reason.SendKeys("Test");
+                APEM.MocmainWindow.ConfirmDialog.OK.Click();
+                DateTime RPLcertify = DateTime.Now;
+                //Console.WriteLine(RPLcertify);
+                Thread.Sleep(3000);
+                APEM.MocmainWindow.RPLDesign.ClickSignle();
+                Thread.Sleep(5000);
+                APEM.MocmainWindow.RPLDesignInternalFrame.RPLListTable.Row(RPLname).DoubleClick();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.GetSnapshot(Resultpath + "RPLDate.PNG");
+                DateTime RPLverifydate = DateTime.Parse(APEM.MocmainWindow.RPLManagementInternalFrame.verifyDate.Text);
+                DateTime RPLcertifydate = DateTime.Parse(APEM.MocmainWindow.RPLManagementInternalFrame.certifyDate.Text);
+                TimeSpan timeDifference3 = RPLcertifydate.Subtract(RPLcertify);
+                Base_Assert.IsTrue(timeDifference3.TotalSeconds <= 2);
+                TimeSpan timeDifference4 = RPLcertifydate.Subtract(RPLcertify);
+                Base_Assert.IsTrue(timeDifference4.TotalSeconds <= 2);
+
+                //MRverify
+                APEM.MocmainWindow.MasterRecipes.Click();
+                Thread.Sleep(5000);
+                APEM.MocmainWindow.MasterRecipeInterFrame.MRListTable.Row(MRname).Click();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.MasterRecipeInterFrame.VerifyButton.Click();
+                APEM.CheckParametersDialog.OKButton.Click();
+                APEM.VerifyDialog.NoButton.Click();
+                APEM.MocmainWindow.ConfirmDialog.PasswordEditor.SendKeys(PassWord.qaone1);
+                APEM.MocmainWindow.ConfirmDialog.Reason.SendKeys("Test");
+                APEM.MocmainWindow.ConfirmDialog.OK.Click();
+                DateTime MRverify = DateTime.Now;
+                //Console.WriteLine(MRverify);
+                Thread.Sleep(3000);
+                //MRcertify
+                APEM.MocmainWindow.MasterRecipes.Click();
+                Thread.Sleep(5000);
+                APEM.MocmainWindow.MasterRecipeInterFrame.MRListTable.Row(MRname).Click();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.MasterRecipeInterFrame.CertifyButton.Click();
+                APEM.CheckParametersDialog.OKButton.Click();
+                APEM.CertifyDialog.YesButton.Click();
+                APEM.MocmainWindow.ConfirmDialog.PasswordEditor.SendKeys(PassWord.qaone1);
+                APEM.MocmainWindow.ConfirmDialog.Reason.SendKeys("Test");
+                APEM.MocmainWindow.ConfirmDialog.OK.Click();
+                DateTime MRcertify = DateTime.Now;
+                //Console.WriteLine(MRcertify);
+                Thread.Sleep(3000);
+                APEM.MocmainWindow.MasterRecipes.Click();
+                Thread.Sleep(5000);
+                APEM.MocmainWindow.MasterRecipeInterFrame.MRListTable.Row(MRname).DoubleClick();
+                Thread.Sleep(2000);
+                APEM.MocmainWindow.GetSnapshot(Resultpath + "MRDate.PNG");
+                DateTime MRverifydate = DateTime.Parse(APEM.MocmainWindow.MasterRecipeDataInterFrame.verifyDate.Text);
+                DateTime MRcertifydate = DateTime.Parse(APEM.MocmainWindow.MasterRecipeDataInterFrame.certifyDate.Text);
+                TimeSpan timeDifference5 = MRcertifydate.Subtract(MRcertify);
+                Base_Assert.IsTrue(timeDifference5.TotalSeconds <= 2);
+                TimeSpan timeDifference6 = MRcertifydate.Subtract(MRcertify);
+                Base_Assert.IsTrue(timeDifference6.TotalSeconds <= 2);
+            }
+            finally
+            {
+                LogStep(@"6.Restone config key ");
+                Base_Function.EditConfigKey(Configpath, ConfigKey);
+                //codify all
+                Base_Test.LaunchApp(Base_Directory.Codify_all);
+            }
+
+        }
+
+    }
+}
---
+++ MES_APEM_UFT_Selenium_Auto/MES_APEM_UFT_Selenium_Auto/TestCase/APEM Cases/520174.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MES_APEM_UFT_Selenium_Auto.Library.BaseLibrary;
+using MES_APEM_UFT_Selenium_Auto.Product.APEM;
+using System;
+using HP.LFT.SDK;
+using MES_APEM_UFT_Selenium_Auto.Library.SeleniumLibrary;
+using MES_APEM_UFT_Selenium_Auto.Product.ApemMobile;
+using MES_APEM_UFT_Selenium_Auto.Product.APEM.MOC_TemplatesModule;
+
+namespace MES_APEM_UFT_Selenium_Auto.TestCase
+{
+    public partial class APEM_TestCase
+    {
+        [TestCaseID(520174)]
+        [Title("SET_ORDER_DETAILS and SET_ORDER_DETAILS_EX function work well")]
+        [TestCategory(ProductArea.API)]
+        [Priority(CasePriority.High)]
+        [TestCategory(CaseState.Accepted)]
+        [TestCategory(AutomationTool.UFT_Selenium)]
+        [Owner(AutomationEngineer.Ziru)]
+        [Timeout(1500000)]
+
+        [TestMethod]
+        public void VSTS_520174()
+        {
+            string Resultpath = Base_Directory.ResultsDir + CaseID + "-";
+            Thread.Sleep(2000);
+            string TestRPLname = "FOR_STATUS";
+            string TestOrdername1 = "Test001";
+            string TestOrdername2 = "Test002";
+            string TestOrdername3 = "Test005";
+            string TestOrdername4 = "Test006";
+            string BPLname = "BPL520174";
+            string TestBPLname = "BPL520174_1";
+            string RPLname = "RPL520174";
+            string Ordername = "ORDER520174";
+            Library.BaseLibrary.Application.LaunchMocAndLogin();
+            APEM.MocmainWindow.BPLDesign.ClickSignle();
+            if (!APEM.MocmainWindow.BPLListInternalFrame.BPLList_Table.Row(BPLname).Existing)
+            {
+                MOC_TemplatesFunction.Importtemplates("TEMP520174.zip");
+            }
+            MOC_Fuction.PlanFromRPL(TestRPLname, TestOrdername1, false);
+            MOC_Fuction.PlanFromRPL(TestRPLname, TestOrdername2, false);
+            MOC_Fuction.PlanFromRPL(TestRPLname, TestOrdername3, false);
+            MOC_Fuction.PlanFromRPL(TestRPLname, TestOrdername4, false);
+            MOC_Fuction.PlanFromRPL(RPLname, Ordername);
+            APEM.MocmainWindow.BPLDesign.Click();
+            Thread.Sleep(2000);
+            APEM.MocmainWindow.BPLListInternalFrame.BPLList_Table.Row(TestBPLname, "Name").Click();
+            Thread.Sleep(2000);
+            APEM.MocmainWindow.BPLListInternalFrame.LoadDesigner_Button.Click();
+            Thread.Sleep(1000);
+            if (APEM.MocmainWindow.ReadOnly_Dialog.IsExist())
+            {
+                APEM.MocmainWindow.ReadOnly_Dialog.OKButton.Click();
+            }
+            Thread.Sleep(1000);
+            APEM.DesignEditorWindow.ExecuteButton.ClickSignle();
+            Thread.Sleep(5000);
+            //detail
+            APEM.DesignEditorWindow.ExecuteMainInternalFrame.orderDetailButton.Click();
+            Thread.Sleep(1000);
+            APEM.DesignEditorWindow.MessageInterFrame.OKButton.Click();
+            //detail_ex
+            APEM.DesignEditorWindow.ExecuteMainInternalFrame.orderdetail_EXButton.Click();
+            Thread.Sleep(1000);
+            APEM.DesignEditorWindow.MessageInterFrame.OKButton.Click();
+            APEM.DesignEditorWindow.GetSnapshot(Resultpath + "BPExecute.PNG");
+            APEM.DesignEditorWindow.ExecuteMainInternalFrame._UFT_InterFrame.Close();
+            APEM.MocmainWindow.ExeCancelDialog.YesButton.Click();
+            MOC_Fuction.DesignEditorClose();
+            APEM.MocmainWindow.Orders.ClickSignle();
+            Thread.Sleep(2000);
+            MOC_Fuction.CheckRowSelection();
+            //if exit order cancel it
+            APEM.MocmainWindow.OrderListInternalFrame.Search.SetText(TestOrdername3);//filter order
+            APEM.MocmainWindow.OrderListInternalFrame.Filter_Button.Click();
+            Thread.Sleep(2000);
+            Base_Assert.AreEqual(APEM.MocmainWindow.OrderListInternalFrame.OrderList_Table.GetCell(0, "Description").Value, "test for API function");
+            APEM.MocmainWindow.OrderListInternalFrame.Search.SetText(TestOrdername4);//filter order
+            APEM.MocmainWindow.OrderListInternalFrame.Filter_Button.Click();
+            Thread.Sleep(2000);
+            Base_Assert.AreEqual(APEM.MocmainWindow.OrderListInternalFrame.OrderList_Table.GetCell(0, "Description").Value, "test for API function");
+            APEM.MocmainWindow.WorkstationBP.ClickSignle();
+            MOC_Fuction.CheckRowSelection();
+            Thread.Sleep(2000);
+            APEM.MocmainWindow.WorkstationBPInternalFrame.OrderEditor.SetText(Ordername);
+            APEM.MocmainWindow.WorkstationBPInternalFrame.Filterbutton.Click();
+            APEM.MocmainWindow.WorkstationBPInternalFrame.OrderTable.Row("Ready for execution", "Status").Click();
+            APEM.MocmainWindow.WorkstationBPInternalFrame.ExecuteButton.ClickSignle();
+            Thread.Sleep(4000);
+            APEM.PhaseExecWindow.ExecutionInternalFrame.orderDetailButton.Click();
+            APEM.PhaseExecWindow.MessageInternalFrame.OKButton.Click();
+            //detail_ex
+            APEM.PhaseExecWindow.ExecutionInternalFrame.orderdetail_EXButton.Click();
+            Thread.Sleep(1000);
+            APEM.PhaseExecWindow.MessageInternalFrame.OKButton.Click();
+            APEM.PhaseExecWindow.GetSnapshot(Resultpath + "orderExecute.PNG");
+            APEM.PhaseExecWindow.ExecutionInternalFrame.Cancel_Button.Click();
+            Thread.Sleep(1000);
+            APEM.PhaseExecWindow.ConfirmationInternalFrame.YesButton.Click();
+            Thread.Sleep(2000);
+            APEM.MocmainWindow.Orders.ClickSignle();
+            Thread.Sleep(2000);
+            MOC_Fuction.CheckRowSelection();
+            //if exit order cancel it
+            APEM.MocmainWindow.OrderListInternalFrame.Search.SetText(TestOrdername1);//filter order
+            APEM.MocmainWindow.OrderListInternalFrame.Filter_Button.Click();
+            Thread.Sleep(2000);
+            Base_Assert.AreEqual(APEM.MocmainWindow.OrderListInternalFrame.OrderList_Table.GetCell(0, "Description").Value, "test for API function");
+            APEM.MocmainWindow.OrderListInternalFrame.Search.SetText(TestOrdername2);//filter order
+            APEM.MocmainWindow.OrderListInternalFrame.Filter_Button.Click();
+            Thread.Sleep(2000);
+            Base_Assert.AreEqual(APEM.MocmainWindow.OrderListInternalFrame.OrderList_Table.GetCell(0, "Description").Value, "test for API function");
+
+
+
+        }
+
+    }
+}
